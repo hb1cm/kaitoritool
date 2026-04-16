7781a546995660ee0b23de573d57021b0c7e9bd8
tatsukoohno1441 <tatsukoohno@gmail.com>
2026-04-16 15:50:28 +0900

    “更新商店1”

--- kaitoritool.py
+++ kaitoritool.py
@@ -226,12 +226,14 @@ with tab_compare:
         # --- 🌐 他サイトへのクイックリンク ---
         st.write("### 🔗 他サイトの検索リンク")
         links = {
+            "🏮 買取商店": f"https://www.kaitorishouten-co.jp/products/list?name={compare_jan}",
             "🌐 買取Wiki": f"https://gamekaitori.jp/search?type=&q={compare_jan}#searchtop",
-            "📦 森森買取": f"https://www.mori-mori.jp/search/?keyword={compare_jan}",
-            "📱 じゃんぱら": f"https://www.janpara.co.jp/buy/search/result/?KEYWORD={compare_jan}&ORDER=1",
-            "💻 イオシス": f"https://k-tai-iosys.com/buy/search/result/?KEYWORD={compare_jan}",
+            "📦 森森買取": f"https://www.morimori-kaitori.jp/search?sk={compare_jan}",
+            # "📱 じゃんぱら (Janpara)": f"https://www.janpara.co.jp/buy/search/result/?ORDER=1&KEYWORD={compare_jan}",
+            # "💻 イオシス (Iosys)": f"https://k-tai-iosys.com/search?q={compare_jan}",
             "📉 価格.com": f"https://kakaku.com/search_results/{compare_jan}/"
         }
+
         
         col_l, col_r = st.columns(2)
         for i, (site, url) in enumerate(links.items()):
